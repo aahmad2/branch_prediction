af252f6bf979132597b19cb0431b7f92ab4bcdc5
Sydney Hildreth <srhildre@uvm.edu>
2022-04-05 21:19:48 -0400

    started working on static branch

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import math
+def main():
+    # user specifies:
+        # input trace file
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    try:
+        # have the user enter a file name
+
+        #filename = input("Welcome. Enter a filename: ")
+        filename = 'gcc.trace.out'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    except IOError:
+        # prints invalid file if the file does not exist for reading
+        print("Invalid file.")
+        filename = input("Enter a filename: ")
+        # passes the value of filename to the function num_of_characters so the rest can execute
+
+    # open the file
+    outfile = open(filename, 'r')
+    # read the file
+    #data = outfile.read()
+
+    # convert data into a string
+    #data = str(data)
+
+    # number of bits: 0, 1, 2, or 3
+    num_bits = int(input("Number of bits to use: "))
+
+    # size of branch prediction buffer (assert N  to be a power of 2)
+    N = int(input("Specify size of the branch prediction buffer: "))
+    #assert N is math.pow()
+
+    array = []
+
+    #static BP
+    if (num_bits == 0):
+        for i in range (0, N):
+            array.append(0)
+            address = outfile.readline(i)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+    # tally results across all branches and report:
+        # total number of branches
+        # percentage of correct predictions
+        # percentage of incorrect predictions
+
+
+
+
+
+    # close the file
+    outfile.close()
+
+
+main()
