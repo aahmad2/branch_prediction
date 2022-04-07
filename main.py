e40a7ddc19d02d82e8b9db685b3e1d42964d72f1
Sydney Hildreth <srhildre@uvm.edu>
2022-04-07 13:25:30 -0400

    one-bit BP works!

--- main.py
+++ main.py
@@ -3,40 +3,37 @@ def main():
 
     try:
         # have the user enter a file name
-
         #filename = input("Welcome. Enter a filename: ")
         filename = 'gcc.trace.out'
-
-
     except IOError:
         # prints invalid file if the file does not exist for reading
         print("Invalid file.")
         filename = input("Enter a filename: ")
-        # passes the value of filename to the function num_of_characters so the rest can execute
 
     # open the file
     outfile = open(filename, 'r')
 
     # number of bits: 0, 1, 2, or 3
     num_bits = int(input("Number of bits to use: "))
-#    num_bits = 1
+
+    # num_bits = 1
 
     # size of branch prediction buffer (assert N  to be a power of 2)
     #N = int(input("Specify size of the branch prediction buffer: "))
     N = 128
-    mask = N-1
-    # TODO: assert N is math.pow()
+    mask = N
+    # TODO: assert N is math.pow(N,2)
 
     counter = []
 
     total_predictions = 0
     success = 0
-    for i in range (1, N):
+
+    for i in range (0, N):
         counter.append(0)
+
     #static BP
     if (num_bits == 0):
-
-
         for line in outfile:
             if line != '#eof\n':
                 split_line = line.split()
@@ -45,13 +42,9 @@ def main():
                 total_predictions +=1
                 if counter[address%mask] == bit:
                     success +=1
+        print(success, total_predictions)
         print(success/total_predictions)
-
-
-
-
     elif (num_bits == 1):
-
         for line in outfile:
             if line != '#eof\n':
                 split_line = line.split()
@@ -64,9 +57,6 @@ def main():
         print(success, total_predictions)
         print(success/total_predictions)
 
-
-
-
     elif (num_bits == 2):
         pass
     elif (num_bits == 3):
@@ -78,8 +68,6 @@ def main():
         # total number of branches
         # percentage of correct predictions
         # percentage of incorrect predictions
-
-
 
 
 
