6d5a19420ebff3fed423650f235d82d7e65c9aff
Sydney Hildreth <srhildre@uvm.edu>
2022-04-08 14:00:54 -0400

    added some input error handling and cleaned up code a little

--- main.py
+++ main.py
@@ -1,134 +1,177 @@
-import math
 def main():
 
-    try:
-        # have the user enter a file name
-        #filename = input("Welcome. Enter a filename: ")
-        filename = 'gcc.trace.out'
-    except IOError:
-        # prints invalid file if the file does not exist for reading
-        print("Invalid file.")
-        filename = input("Enter a filename: ")
+    while True:
+        try:
+            # have the user enter a file name
+            filename = input("Please enter a filename: ")
 
-    # open the file
-    outfile = open(filename, 'r')
+            # open the file
+            outfile = open(filename, 'r')
+            break
 
-    # number of bits: 0, 1, 2, or 3
-    num_bits = int(input("Number of bits to use: "))
+        except IOError:
+            # prints invalid file if the file does not exist for reading
+            print("Invalid filename. Please try again.")
+            filename = input("Enter a filename: ")
 
-    # num_bits = 1
 
-    # size of branch prediction buffer (assert N  to be a power of 2)
-    #N = int(input("Specify size of the branch prediction buffer: "))
-    N = 128
+
+    # input error handling for number of bits
+    while True:
+        try:
+            # number of bits: 0, 1, 2, or 3
+            num_bits = int(input("Number of bits to use: "))
+
+            if num_bits < 0 or num_bits > 3:
+                raise ValueError
+            break
+
+        except ValueError:
+            print("This is not a valid number. Please insert a number 0-3")
+
+
+    # input error handling for size of N
+    while True:
+        try:
+            # The value of N must be a power of 2
+            N = int(input("Specify size of the branch prediction buffer: "))
+            assert (N & (N - 1) == 0) and N != 0,AssertionError
+            break
+        except AssertionError:
+            print("Size must be power of 2")
+        except ValueError:
+            print("Must enter a value.")
+
+
     mask = N
-    # TODO: assert N is math.pow(N,2)
-
     counter = []
 
     total_predictions = 0
     success = 0
+    fail = 0
 
     for i in range (0, N):
         counter.append(0)
 
     #static BP
     if (num_bits == 0):
+
         for line in outfile:
             if line != '#eof\n':
+
                 split_line = line.split()
-                address = int(split_line[0],16)
+                address = int(split_line[0], 16)
                 bit = int(split_line[1], 2)
-                total_predictions +=1
-                if counter[address%mask] == bit:
-                    success +=1
-        print(success, total_predictions)
-        print(success/total_predictions)
+                total_predictions += 1
 
+                if counter[address % mask] == bit:
+                    success += 1
+                else:
+                    fail += 1
 
-
+        # tally results across all branches and report:
+            # total number of branches
+            # percentage of correct predictions
+            # percentage of incorrect predictions
+        print("total number of branches:", total_predictions)
+        print("fails: ", '{0:.4g}%'.format(fail/total_predictions * 100))
+        print("success: ",'{0:.4g}%'.format(success/total_predictions * 100))
 
     elif (num_bits == 1):
+
         for line in outfile:
             if line != '#eof\n':
+
                 split_line = line.split()
-                address = int(split_line[0],16)
+                address = int(split_line[0], 16)
                 bit = int(split_line[1], 2)
-                total_predictions +=1
-                if counter[address%mask] == bit:
-                    success +=1
-                counter[address%mask] = bit
-                if counter[address % mask] == 2:
-                    print(counter[address % mask])
-        print(success, total_predictions)
-        print(success/total_predictions)
+                total_predictions += 1
+
+                if counter[address % mask] == bit:
+                    success += 1
+                else:
+                    fail += 1
+
+                counter[address % mask] = bit
 
 
-
-
+        # tally results across all branches and report:
+            # total number of branches
+            # percentage of correct predictions
+            # percentage of incorrect predictions
+        print("total number of branches:", total_predictions)
+        print("fails: ", '{0:.4g}%'.format(fail/total_predictions * 100))
+        print("success: ", '{0:.4g}%'.format(success/total_predictions * 100))
 
     elif (num_bits == 2):
+
         for line in outfile:
             if line != '#eof\n':
+
                 split_line = line.split()
-                address = int(split_line[0],16)
+                address = int(split_line[0], 16)
                 bit = int(split_line[1], 2)
-                total_predictions +=1
+                total_predictions += 1
+
                 if counter[address % mask] == 2 or counter[address % mask] == 3:
                     prediction = 1
-                    #success +=1
-                    #counter[address % mask] = min(counter[address % mask] + 1, 3)
                 else:
                     prediction = 0
-                    #counter[address % mask] = max(counter[address % mask] - 1, 0)
 
                 if bit == 1:
                     counter[address % mask] = min(counter[address % mask] + 1, 3)
                 else:
                     counter[address % mask] = max(counter[address % mask] - 1, 0)
+
                 if prediction == bit:
                     success +=1
+                else:
+                    fail +=1
 
-        print(success, total_predictions)
-        print(success/total_predictions)
-
-
-
-
+        # tally results across all branches and report:
+            # total number of branches
+            # percentage of correct predictions
+            # percentage of incorrect predictions
+        print("total number of branches:", total_predictions)
+        print("fails: ", '{0:.4g}%'.format(fail/total_predictions*100))
+        print("success: ",'{0:.4g}%'.format(success/total_predictions*100))
 
     elif (num_bits == 3):
         for line in outfile:
             if line != '#eof\n':
                 split_line = line.split()
-                address = int(split_line[0],16)
+                address = int(split_line[0], 16)
                 bit = int(split_line[1], 2)
                 total_predictions +=1
-                if counter[address % mask] == 4 or counter[address % mask] == 5 or counter[address % mask] == 6 or counter[address % mask] == 7:
+
+                if counter[address % mask] == 4 or counter[address % mask] == 5 or counter[address % mask] == 6 or \
+                        counter[address % mask] == 7:
                     prediction = 1
-                    #success +=1
-                    #counter[address % mask] = min(counter[address % mask] + 1, 3)
+
                 else:
                     prediction = 0
-                    #counter[address % mask] = max(counter[address % mask] - 1, 0)
 
                 if bit == 1:
                     counter[address % mask] = min(counter[address % mask] + 1, 7)
                 else:
                     counter[address % mask] = max(counter[address % mask] - 1, 0)
+
                 if prediction == bit:
-                    success +=1
+                    success += 1
+                else:
+                    fail += 1
 
-        print(success, total_predictions)
-        print(success/total_predictions)
+        # tally results across all branches and report:
+            # total number of branches
+            # percentage of correct predictions
+            # percentage of incorrect predictions
+        print("total number of branches:", total_predictions)
+        print("fails: ",'{0:.4g}%'.format(fail/total_predictions*100))
+        print("success: ",'{0:.4g}%'.format(success/total_predictions*100,'%'))
 
 
 
 
-
-    # tally results across all branches and report:
-        # total number of branches
-        # percentage of correct predictions
-        # percentage of incorrect predictions
 
 
 
